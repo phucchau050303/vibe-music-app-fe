b7d975c0d70221ad74e00d11604f099bc6bf84f9
Peter Hoang Chau <phcchu88@gmail.com>
2026-04-20 13:55:13 +0700

    refector prompt input and submission logic

--- src/components/PromtWindow.vue
+++ src/components/PromtWindow.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+    import { useMusicStore } from '../stores/musicStore';
     const redirectToExternal= () => {
         window.location.href="https://ko-fi.com/peterclaymore03x"
+    };
+    const store = useMusicStore();
+    const handleInput = (event: Event) => {
+        const target = event.target as HTMLInputElement;
+        store.setPromt(target.value);
     };
 </script>
 
@@ -9,10 +15,11 @@
         <div class="w-100" style="max-width: 900px; padding: 0 15px;">
             <div class="text-center">
                 <h2 class="mb-4">What Kind Of Music Are You Thinking Of?</h2>
-                <form>
+                <form @submit.prevent="store.submitRequest">
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control rounded-5 shadow-lg p-3 mb-5" placeholder="Tell me what kind of music you want to search">
+                        <input @input="handleInput" type="text" class="form-control rounded-5 shadow-lg p-3 mb-5" placeholder="Tell me your vibe">
                     </div>
+                    <button type="submit" class="btn btn-primary">Search</button>
                     <button type="button" @click="redirectToExternal" class="btn btn-primary">Support Me</button>
                 </form>
             </div>
